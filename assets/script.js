cafb59fc563999cb679c37b14365d97fbe45a8e8
ChemaKing13 <jm2000ho@gmail.com>
2023-04-24 17:58:26 -0600

    final commit

--- assets/script.js
+++ assets/script.js
@@ -5,13 +5,15 @@ var searchBtn = document.getElementById("searchBtn");
 //this function called getApi will help us to fetch the info from the weather Api and then will set it on our html
 function getApi() {
     // var cityName = document.getElementById("cityName"); 
-    
+
     //this fetch must include our api key and will get the name of the city according to the user input 
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${newName.value}&appid=90e017b27566304547bf7d5543eef480`)
         .then(response => response.json())
         .then(data => {
             //this loop will retrive the information about temperature 
             for (i=0; i<5; i++) {
+                //first we get the element by id, each day include an id "day1Temp", this method will get the id by the world day and i++
+                //since the retrieved info is shown as an array, we need to asign the parameters we are going to use 
                 document.getElementById("day" +(i+1)+"Temp").innerHTML = "Temp:" + Number(data.list[i].main.temp -288.53).toFixed(1) + "°";
             }
             //display the wind speed 
